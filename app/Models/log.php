d4c666d3d66877c755cad1f9b84a7b2ca125c8e3
timehostgames <timehostgames@gmail.com>
2024-05-17 12:39:00 +0200

    x

---
+++ app/Models/log.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class log extends Model
+{
+    use HasFactory, Notifiable;
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    /**
+     * Get the logs for the user.
+     */
+    public function logs()
+    {
+        return $this->hasMany(Log::class);
+    }
+}
