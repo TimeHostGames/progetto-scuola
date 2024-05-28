8c8899a56e1753d381aa92ecdc08b281ac90106f
cybergaro <francyclimber@gmail.com>
2024-05-28 10:33:42 +0200

    FX

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('body')
 <div class="container">
-    <button type="button" onclick="openGate()" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">TEST CANCELLO</button>
+    <button type="button" onclick="openGate()" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">APRI CANCELLO</button>
 </div>
 @endsection
 
