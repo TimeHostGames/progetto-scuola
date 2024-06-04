f39257c654b767e48e6fbfb170314a3616d99517
cybergaro <francyclimber@gmail.com>
2024-06-04 09:19:11 +0200

    fz

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -162,7 +162,7 @@
                     </li>
                     <li>
                         <a href="/logout" class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
-                            <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 512 512" stroke-width="1.5" stroke="currentColor" aria-hidden="true" width="100%" height="100%">
+                            <svg class="h-6 w-6 shrink-0" fill="#9ca3af" viewBox="0 0 512 512" stroke-width="1.5" stroke="currentColor" aria-hidden="true" width="100%" height="100%">
                                 <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
                             </svg>
                             Logout
