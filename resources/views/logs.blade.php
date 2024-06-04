f3da31e49d1a9d768ed34f310e11a6d4e5ef2e63
cybergaro <francyclimber@gmail.com>
2024-06-04 09:26:12 +0200

    fx

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -13,7 +13,7 @@
         <h1 class="text-base font-semibold leading-6 text-gray-900">Logs</h1>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onclick="openModal()">Aggiungi utente</button>
+        <button type="button" class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-red-500 hover:bg-red-600 hover:shadow-lg" onclick="header.location = '/delete_logs'">Danger</button>
       </div>
     </div>
     <div class="mt-8 flow-root">
