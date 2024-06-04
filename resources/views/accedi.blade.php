4e48afa002e134dec1658453d193a97890d70fb4
timehostgames <timehostgames@gmail.com>
2024-06-04 08:51:26 +0200

    x

--- resources/views/accedi.blade.php
+++ resources/views/accedi.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto w-auto" src="https://www.fermifrascati.edu.it/images/banners/Logo.png" alt="Logo Scuola" style="height: 10em">
+      <img class="mx-auto w-auto" src="/assets/img/Logo_scuola_colorato.png" alt="Logo Scuola" style="height: 10em">
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Accedi al tuo account</h2>
     </div>
   
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <title>@yield('titolo', '')DA SCEGLIERE</title>
+    <title>@yield('titolo', '')Progetto Cancello</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
