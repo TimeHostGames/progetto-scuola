2dbcfa03b91f356f01e499264b308cbd2f274b38
cybergaro <francyclimber@gmail.com>
2024-06-04 09:33:41 +0200

    fx

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -13,6 +13,7 @@
         <h1 class="text-base font-semibold leading-6 text-gray-900">Logs</h1>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onclick="openModal()">Aggiungi utente</button>
         <button type="button" class="block rounded-md bg-red-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600" onclick="header.location='/delete_logs'">Elimina logs</button>
       </div>
     </div>
